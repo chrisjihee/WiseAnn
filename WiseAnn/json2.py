d317e44ba2a6ffde6c69a4d28030d1e282587e07
Jihee Ryu <chrisjihee@etri.re.kr>
2017-10-23 21:55:01 +0900

    Update reset_tasks, export: 데이터 출력 기능과 이전에 태깅된 결과로 리셋하는 기능 추가!

---
+++ WiseAnn/json2.py
@@ -0,0 +1,209 @@
+import json
+from json.encoder import encode_basestring
+
+INFINITY = float('inf')
+FLOAT_REPR = repr
+BASE_INDENT = None
+EXCEPT_KEYS = ["type", "label", "weight", "sent_id"]
+
+
+class WiseJson(json.JSONEncoder):
+    def iterencode(self, o, _one_shot=False):
+        def floatstr(o, allow_nan=self.allow_nan,
+                     _repr=FLOAT_REPR, _inf=INFINITY, _neginf=-INFINITY):
+            if o != o:
+                text = 'NaN'
+            elif o == _inf:
+                text = 'Infinity'
+            elif o == _neginf:
+                text = '-Infinity'
+            else:
+                return _repr(o)
+            if not allow_nan:
+                raise ValueError(
+                    "Out of range float values are not JSON compliant: " +
+                    repr(o))
+            return text
+
+        if self.check_circular:
+            markers = {}
+        else:
+            markers = None
+        _encoder = encode_basestring
+        _iterencode = _make_iterencode(
+            markers, self.default, _encoder, self.indent, floatstr,
+            self.key_separator, self.item_separator, self.sort_keys,
+            self.skipkeys, _one_shot)
+        return _iterencode(o, 0)
+
+
+def _make_iterencode(markers, _default, _encoder, _indent, _floatstr, _key_separator, _item_separator, _sort_keys, _skipkeys, _one_shot):
+    BASE_INDENT = _indent
+
+    def _iterencode_list(lst, _current_indent_level):
+        _indent = BASE_INDENT
+        if len(lst) > 0 and isinstance(lst[0], (int, long, float)):
+            _indent = None
+        if not lst:
+            yield '[]'
+            return
+        if markers is not None:
+            markerid = id(lst)
+            if markerid in markers:
+                raise ValueError("Circular reference detected")
+            markers[markerid] = lst
+        buf = '['
+        if _indent is not None:
+            _current_indent_level += 1
+            newline_indent = '\n' + (' ' * (_indent * _current_indent_level))
+            separator = _item_separator + newline_indent
+            buf += newline_indent
+        else:
+            newline_indent = None
+            separator = _item_separator
+        first = True
+        for value in lst:
+            if first:
+                first = False
+            else:
+                buf = separator
+            if isinstance(value, basestring):
+                yield buf + _encoder(value)
+            elif value is None:
+                yield buf + 'null'
+            elif value is True:
+                yield buf + 'true'
+            elif value is False:
+                yield buf + 'false'
+            elif isinstance(value, (int, long)):
+                yield buf + str(value)
+            elif isinstance(value, float):
+                yield buf + _floatstr(value)
+            else:
+                yield buf
+                if isinstance(value, (list, tuple)):
+                    chunks = _iterencode_list(value, _current_indent_level)
+                elif isinstance(value, dict):
+                    chunks = _iterencode_dict(value, _current_indent_level)
+                else:
+                    chunks = _iterencode(value, _current_indent_level)
+                for chunk in chunks:
+                    yield chunk
+        if newline_indent is not None:
+            _current_indent_level -= 1
+            yield '\n' + (' ' * (_indent * _current_indent_level))
+        yield ']'
+        if markers is not None:
+            del markers[markerid]
+
+    def _iterencode_dict(dct, _current_indent_level):
+        _indent = BASE_INDENT
+        if any([k in dct for k in EXCEPT_KEYS]):
+            _indent = None
+        if not dct:
+            yield '{}'
+            return
+        if markers is not None:
+            markerid = id(dct)
+            if markerid in markers:
+                raise ValueError("Circular reference detected")
+            markers[markerid] = dct
+        yield '{'
+        if _indent is not None:
+            _current_indent_level += 1
+            newline_indent = '\n' + (' ' * (_indent * _current_indent_level))
+            item_separator = _item_separator + newline_indent
+            yield newline_indent
+        else:
+            newline_indent = None
+            item_separator = _item_separator
+        first = True
+        if _sort_keys:
+            items = sorted(dct.items(), key=lambda kv: kv[0])
+        else:
+            items = dct.iteritems()
+        for key, value in items:
+            if isinstance(key, basestring):
+                pass
+            # JavaScript is weakly typed for these, so it makes sense to
+            # also allow them.  Many encoders seem to do something like this.
+            elif isinstance(key, float):
+                key = _floatstr(key)
+            elif key is True:
+                key = 'true'
+            elif key is False:
+                key = 'false'
+            elif key is None:
+                key = 'null'
+            elif isinstance(key, (int, long)):
+                key = str(key)
+            elif _skipkeys:
+                continue
+            else:
+                raise TypeError("key " + repr(key) + " is not a string")
+            if first:
+                first = False
+            else:
+                yield item_separator
+            yield _encoder(key)
+            yield _key_separator
+            if isinstance(value, basestring):
+                yield _encoder(value)
+            elif value is None:
+                yield 'null'
+            elif value is True:
+                yield 'true'
+            elif value is False:
+                yield 'false'
+            elif isinstance(value, (int, long)):
+                yield str(value)
+            elif isinstance(value, float):
+                yield _floatstr(value)
+            else:
+                if isinstance(value, (list, tuple)):
+                    chunks = _iterencode_list(value, _current_indent_level)
+                elif isinstance(value, dict):
+                    chunks = _iterencode_dict(value, _current_indent_level)
+                else:
+                    chunks = _iterencode(value, _current_indent_level)
+                for chunk in chunks:
+                    yield chunk
+        if newline_indent is not None:
+            _current_indent_level -= 1
+            yield '\n' + (' ' * (_indent * _current_indent_level))
+        yield '}'
+        if markers is not None:
+            del markers[markerid]
+
+    def _iterencode(o, _current_indent_level):
+        if isinstance(o, basestring):
+            yield _encoder(o)
+        elif o is None:
+            yield 'null'
+        elif o is True:
+            yield 'true'
+        elif o is False:
+            yield 'false'
+        elif isinstance(o, (int, long)):
+            yield str(o)
+        elif isinstance(o, float):
+            yield _floatstr(o)
+        elif isinstance(o, (list, tuple)):
+            for chunk in _iterencode_list(o, _current_indent_level):
+                yield chunk
+        elif isinstance(o, dict):
+            for chunk in _iterencode_dict(o, _current_indent_level):
+                yield chunk
+        else:
+            if markers is not None:
+                markerid = id(o)
+                if markerid in markers:
+                    raise ValueError("Circular reference detected")
+                markers[markerid] = o
+            o = _default(o)
+            for chunk in _iterencode(o, _current_indent_level):
+                yield chunk
+            if markers is not None:
+                del markers[markerid]
+
+    return _iterencode
--- ZA/views.py
+++ ZA/views.py
@@ -8,6 +8,7 @@ from pandas import read_excel
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
+from WiseAnn.json2 import WiseJson
 from WiseAnn import *
 from WiseAnn.settings import *
 from WiseAnn.views import *
@@ -20,14 +21,22 @@ guide["ZA_no_task"] = "존재하지 않는 문서입니다. 이전 페이지로 
 guide["ZA_no_user"] = "로그인 정보가 없습니다. 페이지를 다시 로드하세요."
 
 
+def set_for_utf8():
+    import sys
+    reload(sys)
+    sys.setdefaultencoding("utf8")
+
+
+set_for_utf8()
+
+
 def read_text(textname, dirname=os.path.join(BASE_DIR, "data/texts")):
     if os.path.isdir(dirname):
-        filename = os.path.join(dirname, textname + ".ann.json")
+        filename = os.path.join(dirname, textname + ".json")
         if os.path.isfile(filename):
-            data = open(filename).read()
-            text = json.loads(data)
-            for sent in text["sents"]:
-                for word in sent["words"]:
+            text = json.load(open(filename))
+            for sent in text["sentence"]:
+                for word in sent["dependency"]:
                     if word["label"].startswith("N"):
                         word["label_color"] = "forestgreen"
                     elif word["label"].startswith("V"):
@@ -40,27 +49,14 @@ def read_text(textname, dirname=os.path.join(BASE_DIR, "data/texts")):
 
 @never_cache
 def task(request, textname):
-    print("textname = " + textname)
     user = auth_user(request)
-    print(">>>>>(1)", user)
-    print(">>>>>(2.1)")
-    print(textname)
-    print(">>>>>(2.2)")
-    msg = "user is none" if user is None else "user is user"
-    print(">>>>>(3.0)")
     if user is not None:
-        print(">>>>>(3)")
         try:
-            print(">>>>>(3.1)")
             text = Text.objects.get(textname=textname)
             textname = text.textname
-            print(">>>>>(3.1.5)")
             textcont = read_text(textname)
-            # print(textcont)
-            print(">>>>>(3.2)")
             task = Task.objects.get(user=user, text=text)
-            # print(task.entities)
-            return render(request, 'ZA_task.html', {"guide": guide["ZA_task"] + msg,
+            return render(request, 'ZA_task.html', {"guide": guide["ZA_task"],
                                                     "title": "[Title]",
                                                     "textname": textname,
                                                     "textcont": textcont,
@@ -68,10 +64,9 @@ def task(request, textname):
                                                     "entities": task.entities,
                                                     "relations": task.relations})
         except (KeyError, Text.DoesNotExist):
-            return render(request, 'ZA_task.html', {"guide": guide["ZA_no_task"] + msg})
+            return render(request, 'ZA_task.html', {"guide": guide["ZA_no_task"]})
     else:
-        print(">>>>>(3.x)")
-        return render(request, 'ZA_task.html', {"guide": guide["ZA_no_user"] + msg})
+        return render(request, 'ZA_task.html', {"guide": guide["ZA_no_user"]})
 
 
 @csrf_exempt
@@ -120,21 +115,18 @@ def save(request, textname):
 @never_cache
 def index(request):
     user = auth_user(request)
-    print(">>>>>(1)", user)
-    msg = "user is none" if user is None else "user is user"
     if user is not None:
         yes = [x.text.textname for x in user.task_set.filter(finished=True)]
         yet = [x.text.textname for x in user.task_set.filter(finished=False)]
         progress = float(len(yes)) / (len(yes) + len(yet)) * 100
         texts_yes_finished = [{"i": i + 1, "name": x, "label": x} for (i, x) in enumerate(yes)]
         texts_yet_finished = [{"i": i + 1, "name": x, "label": x} for (i, x) in enumerate(yet)]
-        return render(request, 'ZA_index.html', {"guide": guide["ZA"] + msg,
+        return render(request, 'ZA_index.html', {"guide": guide["ZA"],
                                                  "progress": progress,
                                                  "texts_yes_finished": texts_yes_finished,
                                                  "texts_yet_finished": texts_yet_finished})
-        # return JsonResponse({"status": 200, "msg": "OK -- OK?"})
     else:
-        return render(request, 'ZA_index.html', {"guide": guide["ZA_no_user"] + msg})
+        return render(request, 'ZA_index.html', {"guide": guide["ZA_no_user"]})
 
 
 def list_files(indir, keyword=None):
@@ -157,124 +149,120 @@ def clone_tagged_za(za1, zi):
 
 
 @csrf_exempt
-def export(request, username):
+def export(request, username, datadir=os.path.join(BASE_DIR, "data/")):
     print("[INIT] export")
     print("=" * 100)
     print(" Data Export : %s" % username)
     print("=" * 100)
 
-    # 42maru = A1 + A2 + B1 + B3
-    # knue = A1 + B1 + B2 + C + D
-    user_files = {
-        "42maru": [f for f in list_files("/WorkData/LangEval/WiseAnnData/data") if "A1" in f or "A2" in f or "B1" in f or "B3" in f],
-        "knue": [f for f in list_files("/WorkData/LangEval/WiseAnnData/data") if "A1" in f or "B1" in f or "C" in f or "D" in f],
-    }
-    if username in user_files:
-        num_pass = 0
-        num_fail = 0
-        passed_texts = OrderedDict()
-        failed_texts = []
-        files = user_files[username]
-        outdir = "/WorkData/LangEval/WiseAnnData/result_" + username
-        if not os.path.isdir(outdir):
-            os.makedirs(outdir)
-
-        for x in [x for x in Task.objects.all() if x.user.username != "ZA.chrisjihee@etri.re.kr"]:
-            if x.relations != "{}":
-                rels = json.loads(x.relations, object_pairs_hook=OrderedDict)
-                fs = [f for f in files if x.text.textname in f]
-                if len(fs) != 1:
-                    num_fail += 1
-                    failed_texts.append(x.text.textname)
-                    print("[FAIL] %s" % x.text.textname)
-                    continue
-                fname = os.path.basename(fs[0])
-                doc = json.load(open(fs[0]), object_pairs_hook=OrderedDict)
-                num_valid_za = 0
-
-                for si, zas in rels.items():
-                    si = int(si)
-                    zi = 0
-                    doc["sentence"][si]["ZA"] = list()
-                    for za in sorted(zas.values(), key=lambda zz: zz["head_wid"] * 1000000000 + zz["ant_sid"] * 1000000 + zz["ant_wid"] * 1000000):
-                        if za["valid"]:
-                            doc["sentence"][si]["ZA"].append(clone_tagged_za(za, zi))
-                            zi += 1
-                            num_valid_za += 1
-
-                json.dump(doc, open(os.path.join(outdir, fname), "w"))
-                print("[PASS] \t%s\t~~~>\t%d" % (x.text.textname, num_valid_za))
-                passed_texts[x.text.textname] = num_valid_za
-                num_pass += 1
-
-        print("[EXIT] export")
-        return JsonResponse({"status": 200, "msg": "OK", "username": username,
-                             "num_pass": num_pass, "passed_texts": passed_texts,
-                             "num_fail": num_fail, "failed_texts": failed_texts}, status=200)
-    else:
+    tasks = read_excel(os.path.join(datadir, "tasks.xlsx"))
+    textnames = [tasks.ix[i]["filename"].split(".")[0] for i in range(len(tasks)) if tasks.ix[i]["ZA.annotator1"] == username] + \
+                [tasks.ix[i]["filename"].split(".")[0] for i in range(len(tasks)) if tasks.ix[i]["ZA.annotator2"] == username]
+    if len(textnames) == 0:
         print("[FAIL] %s : %s" % ("Invalid username", username))
         print("[EXIT] export")
         return JsonResponse({"status": 400, "msg": "Invalid username", "username": username}, status=400)
+    outdir1 = os.path.join(datadir + "result", username)
+    if not os.path.isdir(outdir1):
+        os.makedirs(outdir1)
+    outdir2 = os.path.join(datadir + "tags", username)
+    if not os.path.isdir(outdir2):
+        os.makedirs(outdir2)
+
+    num_pass = 0
+    num_fail = 0
+    for x in [x for x in Task.objects.all() if x.user.username.replace("ZA.guest@home.kr", "ZA.42maru") == username]:
+        if x.relations != "{}":
+            fs = [f for f in textnames if x.text.textname in f]
+            if len(fs) != 1:
+                num_fail += 1
+                print("[FAIL] %s" % x.text.textname)
+                continue
+            fname = os.path.basename(fs[0])
+
+            doc = json.load(open(os.path.join(datadir + "texts", fname + ".json")), object_pairs_hook=OrderedDict)
+            ents = json.loads(x.entities, object_pairs_hook=OrderedDict)
+            rels = json.loads(x.relations, object_pairs_hook=OrderedDict)
+            json.dump(ents, open(os.path.join(outdir2, fname + ".entities"), "w"), ensure_ascii=False, indent=2)
+            json.dump(rels, open(os.path.join(outdir2, fname + ".relations"), "w"), ensure_ascii=False, indent=2)
+
+            num_valid = 0
+            for si, zas in rels.items():
+                si = int(si)
+                zi = 0
+                doc["sentence"][si]["ZA"] = list()
+                for za in sorted(zas.values(), key=lambda zz: zz["head_wid"] * 1000000000 + zz["ant_sid"] * 1000000 + zz["ant_wid"] * 1000000):
+                    if za["valid"]:
+                        doc["sentence"][si]["ZA"].append(clone_tagged_za(za, zi))
+                        zi += 1
+                        num_valid += 1
+
+            json.dump(doc, open(os.path.join(outdir1, fname + ".json"), "w"), ensure_ascii=False, indent=4, cls=WiseJson)
+            print("[PASS] \t%s\t~~~>\t%d" % (x.text.textname, num_valid))
+            num_pass += 1
+
+    print("[EXIT] export")
+    return JsonResponse({"status": 200, "msg": "OK", "username": username, "num_pass": num_pass, "num_fail": num_fail}, status=200)
 
 
 @csrf_exempt
 def reset(request):
     if request.POST.has_key("password") and request.POST["password"] == TRUTH:
-        return JsonResponse({"status": 200, "msg": "OK",
-                             "num_users": reset_users(),
-                             "num_texts": reset_texts(),
-                             "num_tasks": reset_tasks()}, status=200)
+        return JsonResponse({"status": 200, "msg": "OK", "num_users": reset_users(), "num_texts": reset_texts(), "num_tasks": reset_tasks()}, status=200)
     else:
         return JsonResponse({"status": 401, "msg": "Unauthorized"}, status=401)
 
 
-def reset_users(filename=os.path.join(BASE_DIR, "data/users.json")):
+def reset_users(datadir=os.path.join(BASE_DIR, "data/")):
+    users = json.load(open(os.path.join(datadir, "users.json")))
     num = 0
-    if os.path.isfile(filename):
-        for x in User.objects.all():
-            x.delete()
-        data = open(filename).read()
-        for x in json.loads(data):
-            User(app=x["app"],
-                 level=x["level"],
-                 username=x["username"],
-                 password=x["password"],
-                 fullname=x["fullname"],
-                 telephone=x["telephone"]).save()
-            num += 1
-        print(">>> [ZA.views.reset_users] Insert: %d users" % num)
-    else:
-        print(">>> [ZA.views.reset_users] No file: %s" % filename)
+    for x in User.objects.all():
+        x.delete()
+    for x in users:
+        User(app=x["app"],
+             level=x["level"],
+             username=x["username"],
+             password=x["password"],
+             fullname=x["fullname"],
+             telephone=x["telephone"]).save()
+        num += 1
+    print(">>> [ZA.views.reset_users] Insert: %d users" % num)
     return num
 
 
-def reset_texts(dirname=os.path.join(BASE_DIR, "data/texts")):
+def reset_texts(datadir=os.path.join(BASE_DIR, "data/")):
+    texts = sorted(os.listdir(os.path.join(datadir, "texts")))
     num = 0
-    if os.path.isdir(dirname):
-        for x in Text.objects.all():
-            x.delete()
-        for x in sorted(os.listdir(dirname)):
-            Text(textname=x.split(".")[0]).save()
-            num += 1
-        print(">>> [ZA.views.reset_texts] Insert: %d texts" % num)
-    else:
-        print(">>> [ZA.views.reset_texts] No dir: %s" % dirname)
+    for x in Text.objects.all():
+        x.delete()
+    for x in texts:
+        Text(textname=x.split(".")[0]).save()
+        num += 1
+    print(">>> [ZA.views.reset_texts] Insert: %d texts" % num)
     return num
 
 
-def reset_tasks(filename=os.path.join(BASE_DIR, "data/tasks.xlsx")):
-    data = read_excel(filename)
+def reset_tasks(datadir=os.path.join(BASE_DIR, "data/")):
+    tasks = read_excel(os.path.join(datadir, "tasks.xlsx"))
     num = 0
     for x in Task.objects.all():
         x.delete()
     for x in User.objects.all():
-        _, annotator = x.username.split(".")
-        textnames1 = [(data.ix[i]["filename"].split(".")[0], data.ix[i]["finished1"]) for i in range(len(data)) if data.ix[i]["annotator1"] == annotator]
-        textnames2 = [(data.ix[i]["filename"].split(".")[0], data.ix[i]["finished2"]) for i in range(len(data)) if data.ix[i]["annotator2"] == annotator]
-        textnames = dict(textnames1 + textnames2)
+        tagsdir = os.path.join(datadir + "tags", x.username)
+        textnames = [(tasks.ix[i]["filename"].split(".")[0], tasks.ix[i]["ZA.finished1"]) for i in range(len(tasks)) if tasks.ix[i]["ZA.annotator1"] == x.username] + \
+                    [(tasks.ix[i]["filename"].split(".")[0], tasks.ix[i]["ZA.finished2"]) for i in range(len(tasks)) if tasks.ix[i]["ZA.annotator2"] == x.username]
+        textnames = dict(textnames)
         for y in Text.objects.all():
             if y.textname in textnames:
-                Task(user=x, text=y, finished=textnames[y.textname]).save()
-                num += 1
+                tagfile1 = os.path.join(tagsdir, y.textname + ".entities")
+                tagfile2 = os.path.join(tagsdir, y.textname + ".relations")
+                if os.path.isfile(tagfile1) and os.path.isfile(tagfile2):
+                    tags1 = open(tagfile1).read()
+                    tags2 = open(tagfile2).read()
+                    Task(user=x, text=y, finished=textnames[y.textname], entities=tags1, relations=tags2).save()
+                    num += 1
+                else:
+                    Task(user=x, text=y, finished=textnames[y.textname]).save()
+                    num += 1
     print(">>> [ZA.views.reset_tasks] Insert: %d tasks" % num)
     return num
